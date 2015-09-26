a4b0a1cf121cf33367591efa7a06d4e6c5c18d3c
LLCoolDave <llcooldave@gmail.com>
2015-09-26 16:55:43 +0200

    Added visit command

--- MafiaBotModule.py
+++ MafiaBotModule.py
@@ -58,7 +58,7 @@ def AdminOnly(bot, trigger):
             bot.say(response, max_messages=10)
 
 
-@module.commands('shoot', 'check', 'protect', 'block', 'use', 'pass')
+@module.commands('shoot', 'check', 'protect', 'block', 'use', 'pass', 'visit')
 @module.require_privmsg()
 def PlayerAction(bot, trigger):
     response = bot.memory['MafiaBotDir'].HandlePlayerCommand(trigger.group(1), trigger.sender, trigger.nick, trigger.group(2), bot)
